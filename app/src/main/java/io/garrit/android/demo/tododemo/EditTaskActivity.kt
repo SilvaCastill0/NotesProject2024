564f10c653efd194633f6188ee0bc7a04ce1bdb1
SilvaCastill0 <sich23ll@student.ju.se>
2024-11-12 23:09:07 +0100

    TopAppBar
    Implemented a back button

--- app/src/main/java/io/garrit/android/demo/tododemo/EditTaskActivity.kt
+++ app/src/main/java/io/garrit/android/demo/tododemo/EditTaskActivity.kt
@@ -6,13 +6,21 @@ import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.*
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.runtime.*
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.Alignment
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
 
 class EditTaskActivity : ComponentActivity() {
@@ -31,55 +39,86 @@ class EditTaskActivity : ComponentActivity() {
         }
     }
 
+    @OptIn(ExperimentalMaterial3Api::class)
     @Composable
     fun EditTaskScreen(task: Task) {
-        var newTitle = remember { mutableStateOf(task.title) }
-        var newDescription = remember { mutableStateOf(task.description) }
-        val context = LocalContext.current
+        val newTitle = remember { mutableStateOf(task.title) }
+        val newDescription = remember { mutableStateOf(task.description) }
+        val context = LocalContext.current as ComponentActivity
 
         Column(
-            modifier = Modifier
-                .padding(16.dp)
-                .fillMaxSize()
+            modifier = Modifier.fillMaxSize()
         ) {
-            OutlinedTextField(
-                value = newTitle.value,
-                onValueChange = { newTitle.value = it },
-                label = { Text("Title") },
-                modifier = Modifier.fillMaxWidth()
-            )
-            OutlinedTextField(
-                value = newDescription.value,
-                onValueChange = { newDescription.value = it },
-                label = { Text("Description") },
-                modifier = Modifier
-                    .fillMaxWidth()
-                    .padding(top = 8.dp)
-                    .weight(1f)
-            )
-
-            Spacer(modifier = Modifier.weight(0.1f))
-
-            Button(
-                onClick = {
-                    if (newTitle.value.isEmpty() || newDescription.value.isEmpty()) {
-                        Toast.makeText(context, "Please do not leave fields empty", Toast.LENGTH_SHORT).show()
-                    } else if(newTitle.value.length < 3 || newDescription.value.length < 5) {
-                        Toast.makeText(context, "Description must be at least 10 characters and Title must be at least 3 characters", Toast.LENGTH_SHORT).show()
-                    } else {
-                        val intent = Intent().apply {
-                            putExtra("task_title", newTitle.value)
-                            putExtra("task_description", newDescription.value)
-                        }
-                        setResult(RESULT_OK, intent)
-                        finish()
+            TopAppBar(
+                title = { Text("Edit Task") },
+                navigationIcon = {
+                    IconButton(onClick = { finish() }) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBack,
+                            contentDescription = "Back",
+                            tint = Color.White
+                        )
                     }
                 },
+                colors = TopAppBarDefaults.smallTopAppBarColors(
+                    containerColor = Color.DarkGray,
+                    titleContentColor = Color.White,
+                    navigationIconContentColor = Color.White
+                )
+            )
+
+            Column(
                 modifier = Modifier
-                    .align(Alignment.CenterHorizontally)
-                    .padding(top = 16.dp)
+                    .padding(16.dp)
+                    .fillMaxSize()
             ) {
-                Text("Save")
+                OutlinedTextField(
+                    value = newTitle.value,
+                    onValueChange = { newTitle.value = it },
+                    label = { Text("Title") },
+                    modifier = Modifier.fillMaxWidth()
+                )
+                OutlinedTextField(
+                    value = newDescription.value,
+                    onValueChange = { newDescription.value = it },
+                    label = { Text("Description") },
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .padding(top = 8.dp)
+                        .weight(1f)
+                )
+
+                Spacer(modifier = Modifier.weight(0.1f))
+
+                Button(
+                    onClick = {
+                        if (newTitle.value.isEmpty() || newDescription.value.isEmpty()) {
+                            Toast.makeText(
+                                context,
+                                "Please do not leave fields empty",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else if (newTitle.value.length < 3 || newDescription.value.length < 5) {
+                            Toast.makeText(
+                                context,
+                                "Description must be at least 10 characters and Title must be at least 3 characters",
+                                Toast.LENGTH_SHORT
+                            ).show()
+                        } else {
+                            val intent = Intent().apply {
+                                putExtra("task_title", newTitle.value)
+                                putExtra("task_description", newDescription.value)
+                            }
+                            setResult(RESULT_OK, intent)
+                            finish()
+                        }
+                    },
+                    modifier = Modifier
+                        .align(Alignment.CenterHorizontally)
+                        .padding(top = 16.dp)
+                ) {
+                    Text("Save")
+                }
             }
         }
     }
--- app/src/main/java/io/garrit/android/demo/tododemo/TaskActivity.kt
+++ app/src/main/java/io/garrit/android/demo/tododemo/TaskActivity.kt
@@ -23,6 +23,13 @@ import io.garrit.android.demo.tododemo.ui.theme.TodoDemoTheme
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.ui.Alignment
 import android.widget.Toast
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
 import androidx.compose.ui.platform.LocalContext
 
 
@@ -49,50 +56,83 @@ class TaskActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun TaskInputScreen(onSubmit: (String, String) -> Unit) {
     val titleState = remember { mutableStateOf("") }
     val descriptionState = remember { mutableStateOf("") }
-    val context = LocalContext.current
+    val context = LocalContext.current as ComponentActivity
+
+
 
     Column(
-        modifier = Modifier
-            .padding(16.dp)
-            .fillMaxSize()
-    ){
-        OutlinedTextField(
-            value = titleState.value,
-            onValueChange = { titleState.value = it },
-            label = { Text("Title", color = Color.Black) },
-            modifier = Modifier.fillMaxWidth()
-        )
-        OutlinedTextField(
-            value = descriptionState.value,
-            onValueChange = { descriptionState.value = it },
-            label = { Text("Description", color = Color.Black) },
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(top = 8.dp)
-                .weight(1f)
-        )
-
-        Spacer(modifier = Modifier.weight(0.1f))
-
-        Button(
-            onClick = {
-                if (titleState.value.isEmpty() || descriptionState.value.isEmpty()) {
-                    Toast.makeText(context, "Please do not leave fields empty", Toast.LENGTH_SHORT).show()
-                } else if (titleState.value.length < 3 || descriptionState.value.length < 5) {
-                    Toast.makeText(context, "Title must be at least 3 characters and Description must be at least 10 characters", Toast.LENGTH_SHORT).show()
-                } else{
-                    onSubmit(titleState.value, descriptionState.value)
+        modifier = Modifier.fillMaxSize()
+    ) {
+        TopAppBar(
+            title = { Text("Edit Task") },
+            navigationIcon = {
+                IconButton(onClick = { context.finish() }) {
+                    Icon(
+                        imageVector = Icons.Filled.ArrowBack,
+                        contentDescription = "Back",
+                        tint = Color.White
+                    )
                 }
             },
+            colors = TopAppBarDefaults.smallTopAppBarColors(
+                containerColor = Color.DarkGray,
+                titleContentColor = Color.White,
+                navigationIconContentColor = Color.White
+            )
+        )
+
+        Column(
             modifier = Modifier
-                .align(Alignment.CenterHorizontally)
-                .padding(top = 16.dp)
-        ){
-            Text("Add Task", color = Color.White)
+                .padding(16.dp)
+                .fillMaxSize()
+        ) {
+            OutlinedTextField(
+                value = titleState.value,
+                onValueChange = { titleState.value = it },
+                label = { Text("Title", color = Color.Black) },
+                modifier = Modifier.fillMaxWidth()
+            )
+            OutlinedTextField(
+                value = descriptionState.value,
+                onValueChange = { descriptionState.value = it },
+                label = { Text("Description", color = Color.Black) },
+                modifier = Modifier
+                    .fillMaxWidth()
+                    .padding(top = 8.dp)
+                    .weight(1f)
+            )
+
+            Spacer(modifier = Modifier.weight(0.1f))
+
+            Button(
+                onClick = {
+                    if (titleState.value.isEmpty() || descriptionState.value.isEmpty()) {
+                        Toast.makeText(
+                            context,
+                            "Please do not leave fields empty",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else if (titleState.value.length < 3 || descriptionState.value.length < 5) {
+                        Toast.makeText(
+                            context,
+                            "Title must be at least 3 characters and Description must be at least 10 characters",
+                            Toast.LENGTH_SHORT
+                        ).show()
+                    } else {
+                        onSubmit(titleState.value, descriptionState.value)
+                    }
+                },
+                modifier = Modifier
+                    .align(Alignment.CenterHorizontally)
+                    .padding(top = 16.dp)
+            ) {
+                Text("Add Task", color = Color.White)
+            }
         }
     }
 }
